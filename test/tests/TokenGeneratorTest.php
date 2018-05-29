351b9e002adec14267fd24551ddf03a5ccb56c24
peter279k <peter279k@gmail.com>
2018-05-29 15:57:16 +0800

    Test enhancement

--- test/tests/TokenGeneratorTest.php
+++ test/tests/TokenGeneratorTest.php
@@ -13,11 +13,11 @@ class TokenGeneratorTest extends TestCase
     public function testGenerateToken() {
         $generator = new TokenGenerator();
         $t1 = $generator->generateToken(10);
-        PHPUnit::assertEquals(10, strlen($t1));
+        $this->assertEquals(10, strlen($t1));
 
         $t2 = (new TokenGenerator())->generateToken(60, 'foo');
-        PHPUnit::assertEquals(60, strlen($t2));
-        PHPUnit::assertEquals('foo', substr($t2, 0, 3));
+        $this->assertEquals(60, strlen($t2));
+        $this->assertEquals('foo', substr($t2, 0, 3));
     } 
 
 }
